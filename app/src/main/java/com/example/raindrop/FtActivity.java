17a3228a8386896a09628dd5039c812d77f98431
name <collins.odinga@student.moringaschool.com>
2022-05-31 15:49:00 +0300

    ft: added feature

--- app/src/main/java/com/example/raindrop/FtActivity.java
+++ app/src/main/java/com/example/raindrop/FtActivity.java
@@ -1,4 +1,30 @@
 package com.example.raindrop;
 
-public class FtActivity {
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+
+public class FtActivity extends AppCompatActivity {
+    @BindView(R.id.viewName)TextView mViewname;
+    @BindView(R.id.viewCountry)TextView mViewcountry;
+    @BindView(R.id.listView) ListView mListView;
+
+    private String[] mCountries = {"England", "Spain", "Italy", "Germany", "France", "Portugal", "Scotland"};
+    private String[] mLeagues = {"Premier League", "La liga", "Serie A", "Bundesliga", "Ligue 1", "Primeira Liga", "Scottish Premiership"};
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ft);
+
+        ButterKnife.bind(this);
+        mViewname.setText(getIntent().getStringExtra("name"));
+        mViewcountry.setText(getIntent().getStringExtra("country"));
+        mListView.setAdapter(new CustomAdapter(this, mCountries, mLeagues));
+    }
 }
