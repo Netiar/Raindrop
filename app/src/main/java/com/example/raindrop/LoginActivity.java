f556de7db5c573117e9989784ea68f37b5abf399
name <collins.odinga@student.moringaschool.com>
2022-05-31 08:43:51 +0300

    ft: added feature

--- app/src/main/java/com/example/raindrop/LoginActivity.java
+++ app/src/main/java/com/example/raindrop/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.raindrop.databinding.ActivityLoginBinding;
 import com.google.android.material.button.MaterialButton;
@@ -13,9 +14,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.nameEditText)EditText mName;
-    @BindView(R.id.passwordEditText)EditText mPassword;
-    @BindView(R.id.loginButton)MaterialButton mLoginButton;
+    @BindView(R.id.nameEditText)
+    EditText mName;
+    @BindView(R.id.passwordEditText)
+    EditText mPassword;
+    @BindView(R.id.loginButton)
+    MaterialButton mLoginButton;
 
 
 
@@ -24,6 +28,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(this);
 
     }
 
@@ -34,6 +40,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+       if(mName.getText().toString().equals("admin") && mPassword.getText().toString().equals("admin")){
+            //correct
+            Toast.makeText(this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+           }else
+               //incorrect
+               Toast.makeText(this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
 
     }
 }
