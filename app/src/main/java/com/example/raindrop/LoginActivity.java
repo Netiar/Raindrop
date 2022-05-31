cbd7e1f5e89625a571d86705d54c6abd4e27e0ab
name <collins.odinga@student.moringaschool.com>
2022-05-31 08:34:30 +0300

    ft: added feature

--- app/src/main/java/com/example/raindrop/LoginActivity.java
+++ app/src/main/java/com/example/raindrop/LoginActivity.java
@@ -3,12 +3,37 @@ package com.example.raindrop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.raindrop.databinding.ActivityLoginBinding;
+import com.google.android.material.button.MaterialButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.nameEditText)EditText mName;
+    @BindView(R.id.passwordEditText)EditText mPassword;
+    @BindView(R.id.loginButton)MaterialButton mLoginButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
