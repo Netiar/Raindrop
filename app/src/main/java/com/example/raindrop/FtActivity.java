4cd0e67332498958d0b610f34f669937f874d7f5
name <collins.odinga@student.moringaschool.com>
2022-05-31 16:28:48 +0300

    rf:refactored code

--- app/src/main/java/com/example/raindrop/FtActivity.java
+++ app/src/main/java/com/example/raindrop/FtActivity.java
@@ -1,15 +1,16 @@
 package com.example.raindrop;
 
+import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FtActivity extends AppCompatActivity {
-    @BindView(R.id.viewName)TextView mViewname;
-    @BindView(R.id.viewCountry)TextView mViewcountry;
+
     @BindView(R.id.listView) ListView mListView;
 
     private String[] mCountries = {"England", "Spain", "Italy", "Germany", "France", "Portugal", "Scotland"};
@@ -17,14 +18,12 @@ public class FtActivity extends AppCompatActivity {
 
 
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ft);
 
         ButterKnife.bind(this);
-        mViewname.setText(getIntent().getStringExtra("name"));
-        mViewcountry.setText(getIntent().getStringExtra("country"));
-        mListView.setAdapter(new CustomAdapter(this, mCountries, mLeagues));
+
     }
 }
