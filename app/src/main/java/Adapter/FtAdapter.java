0fd8094cfce42b688c72d5de37331e4231ef2ef5
name <collins.odinga@student.moringaschool.com>
2022-06-01 20:10:09 +0300

    ft: added feature

--- app/src/main/java/Adapter/FtAdapter.java
+++ app/src/main/java/Adapter/FtAdapter.java
@@ -1,4 +1,16 @@
 package Adapter;
 
-public class FtAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import com.example.raindrop.FtActivity;
+
+public class FtAdapter extends ArrayAdapter {
+
+
+    public FtAdapter(@NonNull Context context, int resource, Object countries) {
+        super(context, resource);
+    }
 }
