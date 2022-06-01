fc03d237826e5558435c537c33c58ab6a6e63e84
name <collins.odinga@student.moringaschool.com>
2022-06-01 20:10:37 +0300

    ft: added feature

--- app/src/main/java/com/example/raindrop/FtActivity.java
+++ app/src/main/java/com/example/raindrop/FtActivity.java
@@ -1,20 +1,28 @@
 package com.example.raindrop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import Adapter.FtAdapter;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class FtActivity extends AppCompatActivity {
 
+    @BindView(R.id.countryTextView) TextView mCountryTextView;
     @BindView(R.id.listView) ListView mListView;
 
-    private String[] mCountries = {"England", "Spain", "Italy", "Germany", "France", "Portugal", "Scotland"};
-    private String[] mLeagues = {"Premier League", "La liga", "Serie A", "Bundesliga", "Ligue 1", "Primeira Liga", "Scottish Premiership"};
+    private String[] Countries = new String[] {"England", "Spain", "Italy", "Germany", "France", "Portugal", "Scotland"};
+    private Object countries;
+
+//    private String[] mLeagues = {"Premier League", "La liga", "Serie A", "Bundesliga", "Ligue 1", "Primeira Liga", "Scottish Premiership"};
 
 
 
@@ -22,8 +30,35 @@ public class FtActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ft);
-
         ButterKnife.bind(this);
+
+        Intent intent= getIntent();
+        String country = intent.getStringExtra("country");
+        mCountryTextView.setText("Here are all the countries." );
+
+
+
+
+
+        FtAdapter adapter = new FtAdapter(this, android.R.layout.simple_list_item_1, countries);
+        mListView.setAdapter(adapter);
+
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(FtActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mCountryTextView = (TextView) findViewById(R.id.countryTextView);
+        mCountryTextView.setText(country);
+
+
+
 
     }
 }
