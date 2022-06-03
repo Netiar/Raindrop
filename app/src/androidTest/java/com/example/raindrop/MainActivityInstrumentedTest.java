52fd749df8759d541db295f37ea49806b99eba73
name <collins.odinga@student.moringaschool.com>
2022-06-03 11:45:04 +0300

    added file

--- app/src/androidTest/java/com/example/raindrop/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/raindrop/MainActivityInstrumentedTest.java
@@ -1,4 +1,37 @@
 package com.example.raindrop;
 
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.view.View;
+
+import androidx.test.espresso.matcher.ViewMatchers;import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+
+//    @Test
+//    public void validateButton() {
+//        onView(ViewMatchers.withId(R.id.button)).perform(click());
+//    }
+
+
+
+
 }
