684af8f717707dff60a87a0500afef98bbd25fba
name <collins.odinga@student.moringaschool.com>
2022-06-07 13:42:34 +0300

    added file

---
+++ app/src/main/java/com/example/raindrop/Constants.java
@@ -0,0 +1,7 @@
+package com.example.raindrop;
+
+public class Constants {
+    public static final String BASE_URL = "https://football.elenasport.io/v2/";
+    public static final String API_KEY = "";
+
+}
