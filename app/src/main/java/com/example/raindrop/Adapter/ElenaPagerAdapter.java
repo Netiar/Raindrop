7290f2a8c6e0379c5348ad107486df253df5fb75
name <collins.odinga@student.moringaschool.com>
2022-06-08 08:43:18 +0300

    added file

---
+++ app/src/main/java/com/example/raindrop/Adapter/ElenaPagerAdapter.java
@@ -0,0 +1,29 @@
+package com.example.raindrop.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class ElenaPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments;
+    private List<String> titles;
+
+
+    public ElenaPagerAdapter(@NonNull FragmentManager fm, int behavior, List<Fragment> fragments, List<String> titles) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
