e8f0e1bb07bc1357f0f5171554bae78343839a39
name <collins.odinga@student.moringaschool.com>
2022-06-14 04:58:25 +0300

    ft:added features

--- app/src/main/java/com/example/raindrop/network/FpApi.java
+++ app/src/main/java/com/example/raindrop/network/FpApi.java
@@ -2,28 +2,42 @@ package com.example.raindrop.network;
 
 
 
-import com.example.raindrop.models.Leagues;
 
-import java.util.List;
+
+
+
+import com.example.raindrop.models.Datum;
+import com.example.raindrop.models.Odds;
+import com.example.raindrop.models.Response;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Response;
+
 import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
-import retrofit2.http.POST;
+
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 
-public interface ElenaApi {
-    @POST("token")
-    Call<Response> getToken(@Header("Authorization") String auth);
+public interface FpApi {
+
+    @GET("predictions?market=classic&iso_date=2018-12-01&federation=UEFA")
+    Call<Response> getPrediction();
 
 
-    @GET("leagues")
-    Call<List<Leagues>> getLeagues(@Header("Authorization") String authToken);
+
+
+
+
+
+
+
+
+
+
 
 
 
