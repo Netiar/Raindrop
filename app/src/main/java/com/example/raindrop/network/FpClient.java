87b0e30c0f8143b7b98cb1f34b4d7408d33783c4
name <collins.odinga@student.moringaschool.com>
2022-06-14 04:58:58 +0300

    ft:added features

--- app/src/main/java/com/example/raindrop/network/FpClient.java
+++ app/src/main/java/com/example/raindrop/network/FpClient.java
@@ -4,38 +4,42 @@ import com.example.raindrop.Constants;
 
 import java.io.IOException;
 
-import okhttp3.MediaType;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
-public class ElenaClient {
+public class FpClient {
 
     private static Retrofit retrofit = null;
 
-    public static FpApi getClient() throws IOException {
+    public static FpApi getClient()  {
 
 
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                   .build();
-        MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "");
-        Request request = new Request.Builder()
-                .url("https://football.elenasport.io/v2/leagues?expand=***expand***&name=***name***&page=***page***")
-                .get()
-                .addHeader("Authorization", "Bearer ")
-                .build();
-
-        Response response = client.newCall(request).execute();
 
 
         if (retrofit == null) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request request = chain.request().newBuilder()
+                           .addHeader("X-RapidAPI-Key", Constants.X_RAPIDAPI_KEY)
+                           .addHeader("X-RapidAPI-Host", "football-prediction-api.p.rapidapi.com")
+                           .addHeader("Content-Type", "application/json")
+                           .addHeader("Accept", "application/json")
+                                    .build();
+                            return chain.proceed(request);}
+                    }).build();
+
+
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(Constants.BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
