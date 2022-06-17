d4bc9f80e152b2d2e012345848540adc726e1db7
name <collins.odinga@student.moringaschool.com>
2022-06-17 08:13:06 +0300

    ft:added feature

--- app/src/main/java/com/example/raindrop/Constants.java
+++ app/src/main/java/com/example/raindrop/Constants.java
@@ -5,5 +5,6 @@ package com.example.raindrop;
 public class Constants {
     public static final String BASE_URL = "https://football-prediction-api.p.rapidapi.com/api/v2/";
     public static final String X_RAPIDAPI_KEY =BuildConfig.X_API_Key;
+    public static final String FIREBASE_CHILD_RAINDROP = "raindrop";
 
 }
